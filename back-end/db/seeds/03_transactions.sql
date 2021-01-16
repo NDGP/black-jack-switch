ba4ddff5ee721375b7ab78f3b1b80e48f348d10c
Josh <josh.lerner95@gmail.com>
2021-01-16 18:17:28 +0000

    transactions table and seeds created

---
+++ back-end/db/seeds/03_transactions.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+transactions (ammount, date, user_id)
+VALUES
+(100, current_timestamp, 1),
+(-100, current_timestamp, 2),
+(50, current_timestamp, 1),
+(300, current_timestamp, 3),
+(500, current_timestamp, 4),
+(-25, current_timestamp, 3),
+(50, current_timestamp, 2)
