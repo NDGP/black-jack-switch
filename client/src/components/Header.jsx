0e4826e144a53b1d20c4cba3d1091b98544c41d0
Josh <josh.lerner95@gmail.com>
2021-01-16 17:27:26 +0000

    create dropdown nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import {Link} from 'react-router-dom'
+import "./Header.css";
 
 export default function Header(props){
   return (
-    <nav>
+    <div class="dropdown">
+  <button class="dropbtn">Menu</button>
+  <div id="myDropdown" class="dropdown-content">
+      <Link to="/deposit"> Deposit</Link>
+      <Link to="/withdraw"> Withdraw</Link>
       <Link to="/login"> Login</Link>
-    </nav>
+      <button class="logout">Logout </button>  
+      </div>
+    </div>
   )
-
 }
---
+++ client/src/components/Withdraw.jsx
@@ -0,0 +1,5 @@
+export default function Withdraw(props){
+  return (
+  <div><h1> Would you like to withdraw money? </h1></div>
+  )
+}
