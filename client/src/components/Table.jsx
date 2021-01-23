a30004cbafcd44aef45587c3733d76cc75070215
Josh <josh.lerner95@gmail.com>
2021-01-23 18:05:31 +0000

    re added win/bj counter

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -27,34 +27,43 @@ export default function Table(props) {
     if (hands[index].value > 21) {
       result = "loss"
       message = `BUST`;
+      losses++;
     }
     if (hands[index].value <= 21 && dealer.value > 22) {
       result = "win";
       message = `WIN`
+      wins++;
     }
     if ((dealer.value === 22 && hands[index].value < 21) || (hands[index].value === dealer.value && hands[index].value < 22)) {
       result = "push";
       message = "PUSH"
+      draws++;
     }
     if (hands[index].value === 21 && dealer.value !== 21 && dealer.value !== 22 && hands[index].cards.length === 2) {
       result = "win";
       message = `BLACKJACK`
+      wins++;
+      blackjacks++;
     }    
     if (hands[index].value === 21 && dealer.value !== 21 && dealer.value !== 22 && hands[index].cards.length !== 2 ) {
       result = "win";
       message = `WIN`
+      wins++;
     }
     if (dealer.value === 21 && hands[index].value < 21 ) {
       result = "loss";
       message = `LOSS`
+      losses++;
     }
     if ((21 - hands[index].value) < (21 - dealer.value) && dealer.value <= 20 && hands[index].value <= 20) {
       result = "win"
       message = `WIN`
+      wins++;
     }
     if ((21 - dealer.value) < (21 - hands[index].value) && dealer.value <= 20 && hands[index].value <= 20) {
       result = "loss";
       message = `LOSS`
+      losses++;
     }
     return (
       <Hand
@@ -89,6 +98,11 @@ export default function Table(props) {
   return (
     <section>
       <h1> Blackjack Switch! </h1>
+      {/* <p>Wins: {props.totalWins}</p>
+      <p>Win percentage: {winPercentage()}%</p>
+      <p>Blackjacks: {props.totalBlackjacks}</p>
+      <p>Blackjack percentage: {blackjackPercentage()}%</p> */}
+
       <div id="deck">
         DECK:
           < br />
